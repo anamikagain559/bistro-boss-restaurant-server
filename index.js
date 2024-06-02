7ad9ddbacee687b25134c14552b9564db5ed8490
anamikagain559 <anamika.zaimahtech@gmail.com>
2024-06-02 21:07:45 +0600

    payment added successfully

--- index.js
+++ index.js
@@ -144,15 +144,15 @@ async function run() {
       });
     });
 
-    app.post('/payments', async (req, res) => {
+    app.post("/payments", async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
 
       //  carefully delete each item from the cart
-      console.log('payment info', payment);
+      console.log("payment info", payment);
 
       res.send({ paymentResult });
-    })
+    });
 
     // backend/server.js
     app.get("/surveys", async (req, res) => {
@@ -291,76 +291,6 @@ async function run() {
           .send({ message: "Internal Server Error", error: error.message });
       }
     });
-
-    // // menu related apis
-    // app.get('/menu', async (req, res) => {
-    //   const result = await menuCollection.find().toArray();
-    //   res.send(result);
-    // });
-
-    // app.get('/menu/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await menuCollection.findOne(query);
-    //   res.send(result);
-    // })
-
-    // app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
-    //   const item = req.body;
-    //   const result = await menuCollection.insertOne(item);
-    //   res.send(result);
-    // });
-
-    // app.patch('/menu/:id', async (req, res) => {
-    //   const item = req.body;
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) }
-    //   const updatedDoc = {
-    //     $set: {
-    //       name: item.name,
-    //       category: item.category,
-    //       price: item.price,
-    //       recipe: item.recipe,
-    //       image: item.image
-    //     }
-    //   }
-
-    //   const result = await menuCollection.updateOne(filter, updatedDoc)
-    //   res.send(result);
-    // })
-
-    // app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await menuCollection.deleteOne(query);
-    //   res.send(result);
-    // })
-
-    // app.get('/reviews', async (req, res) => {
-    //   const result = await reviewCollection.find().toArray();
-    //   res.send(result);
-    // })
-
-    // // carts collection
-    // app.get('/carts', async (req, res) => {
-    //   const email = req.query.email;
-    //   const query = { email: email };
-    //   const result = await cartCollection.find(query).toArray();
-    //   res.send(result);
-    // });
-
-    // app.post('/carts', async (req, res) => {
-    //   const cartItem = req.body;
-    //   const result = await cartCollection.insertOne(cartItem);
-    //   res.send(result);
-    // });
-
-    // app.delete('/carts/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await cartCollection.deleteOne(query);
-    //   res.send(result);
-    // })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
